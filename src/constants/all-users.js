fe4d3b039ac74ba46b6b0161206d1b7d77dcb8a8
ROVEN KYLE <ellamateogang@gmail.com>
2022-12-10 01:27:39 +0800

    added pages

---
+++ src/constants/all-users.js
@@ -0,0 +1,125 @@
+
+const all_users = [
+    {
+        id: "#120391823",
+        email: "michael.lawson@reqres.in",
+        first_name: "Michael",
+        last_name: "Bernabe",
+        avatar: "https://reqres.in/img/faces/7-image.jpg",
+        username: "mberna",
+        password: "*******",
+        citations: "254",
+    },
+    {
+        id: "#1039182335",
+        email: "lindsay.ferguson@reqres.in",
+        first_name: "Jayson",
+        last_name: "Fernandez",
+        avatar: "https://reqres.in/img/faces/8-image.jpg",
+        username: "jfern",
+        password: " *******",
+        citations: "254"
+    },
+    {
+        id: "#120351823",
+        email: "tobias.funke@reqres.in",
+        first_name: "John Vincent",
+        last_name: "De perio",
+        avatar: "https://reqres.in/img/faces/9-image.jpg",
+        username: "Jdep",
+        password: "*******",
+        citations: "254",
+    },
+    {
+        id: "#120391827",
+        email: "byron.fields@reqres.in",
+        first_name: "Angela",
+        last_name: "Pamo",
+        avatar: "https://reqres.in/img/faces/10-image.jpg",
+        username: "apamo",
+        password: "*************",
+        citations: "254",
+    },
+    {
+        id: "#120391821",
+        email: "george.edwards@reqres.in",
+        first_name: "Aerold",
+        last_name: "Oduca",
+        avatar: "https://reqres.in/img/faces/11-image.jpg",
+        username: "aoduc",
+        password: " *******",
+        citations: "254",
+    },
+    {
+        id: "#120391832",
+        email: "rachel.howell@reqres.in",
+        first_name: "Rachel",
+        last_name: "Howell",
+        avatar: "https://reqres.in/img/faces/12-image.jpg",
+        username: "rhowel",
+        password: "*******",
+        citations: "254",
+    },
+    {
+        id: "#120391843",
+        email: "michael.lawson@reqres.in",
+        first_name: "Michael",
+        last_name: "Lawson",
+        avatar: "https://reqres.in/img/faces/7-image.jpg",
+        username: "mlaws",
+        password: "*******",
+        citations: "254"
+    },
+    {
+        id: "#120391813",
+        email: "lindsay.ferguson@reqres.in",
+        first_name: "Lindsay",
+        last_name: "Ferguson",
+        avatar: "https://reqres.in/img/faces/8-image.jpg",
+        username: "lferg",
+        password: " *******",
+        citations: "254"
+    },
+    {
+        id: "#120391883",
+        email: "tobias.funke@reqres.in",
+        first_name: "Tobias",
+        last_name: "Funke",
+        avatar: "https://reqres.in/img/faces/9-image.jpg",
+        username: "tfun",
+        password: "*******",
+        citations: "254"
+    },
+    {
+        id: "#120391833",
+        email: "byron.fields@reqres.in",
+        first_name: "Byron",
+        last_name: "Fields",
+        avatar: "https://reqres.in/img/faces/10-image.jpg",
+        username: "bfiel",
+        password: "*************",
+        citations: "254"
+    },
+    {
+        id: "#120391883",
+        email: "george.edwards@reqres.in",
+        first_name: "George",
+        last_name: "Edwards",
+        avatar: "https://reqres.in/img/faces/11-image.jpg",
+        username: "gedwar",
+        password: " *******",
+        citations: "254"
+    },
+    {
+        id: "#120391803",
+        email: "rachel.howell@reqres.in",
+        first_name: "Ryan Cezar",
+        last_name: "Vallo",
+        avatar: "https://reqres.in/img/faces/12-image.jpg",
+        username: "rvall",
+        password: "*******",
+        citations: "254"
+    }
+]
+
+export default all_users;
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import DashboardHeader from '../../components/DashboardHeader'
 import SideBar from '../../components/Sidebar'
 import sidebar_menu from '../../constants/sidebar-menu'
 import '../styles.css'
@@ -6,10 +7,20 @@ import '../styles.css'
 export default function Dashboard() {
   return (
 
-        <div className='dashboard-container'>
-        <SideBar menu={sidebar_menu} />
-          
-          <div className='dashboard-body'></div>
+<div className='dashboard-container'>
+      <SideBar menu={sidebar_menu} />
+      
+      
+      <div className='dashboard-body'>
+      <DashboardHeader
+      title = 'Dashboard'
+      />
+        <div className='dashboard-content-container'>
+        <div className='dashboard-content-header'>
+            <h2>Contents</h2>
           </div>
+        </div>
+      </div>
+    </div>
   )
 }
