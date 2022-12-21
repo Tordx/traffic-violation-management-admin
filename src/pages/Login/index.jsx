e1d1d1b19bc45bde6b0d58fe46ab995e706e88eb
RYAN VALLO <valloryan2@gmail.com>
2022-12-22 00:01:10 +0800

    yes

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -125,7 +125,7 @@ export default function Login(props) {
             <input className='input-box' type = 'password' placeholder='password' onChange={(e) => setPasscode(e.target.value)} />
             {/* <button onClick={setPopup(true)}/> */}
             {/* href = {'/dashboard'} */}
-                <a className='login-text login-button' href = '/dashboard' onClick = {LoginData} >LOG IN</a>
+                <a className='login-text login-button' onClick = {LoginData} >LOG IN</a>
         </div>
     </div>
   )
