3db3a168c10c8fde8aa614e0cf6f5abcafb6d6d9
ROVEN KYLE <ellamateogang@gmail.com>
2023-01-13 02:52:44 +0800

    fixed search method, fetch method and so on

--- src/pages/Dashboard/Citation.js
+++ src/pages/Dashboard/Citation.js
@@ -1,28 +1,22 @@
 import React, {useState, useEffect} from 'react';
 import DashboardHeader from '../../components/DashboardHeader';
-
-import all_violators from '../../constants/all-violators';
 import {calculateRange, sliceData} from '../../utils/table-pagination';
 import PouchDB from 'pouchdb';
 import '../styles.css';
-import DoneIcon from '../../assets/icons/done.svg';
-import CancelIcon from '../../assets/icons/cancel.svg';
-import RefundedIcon from '../../assets/icons/refunded.svg';
 import SideBar from '../../components/Sidebar';
 import sidebar_menu from '../../constants/sidebar-menu';
 
 function Citation () {
     const [search, setSearch] = useState('');
-    const [orders, setOrders] = useState(all_violators);
+    const [content, setContent] = useState('');
     const [page, setPage] = useState(1);
     const [pagination, setPagination] = useState([]);
-    const [status, setStatus] = useState();
 
     useEffect(() => {
-        setPagination(calculateRange(all_violators, 10));
-        setOrders(sliceData(all_violators, page, 10));
+        setPagination(calculateRange(pagination, 5));
+        setContent(sliceData(pagination, page, 5));
         newdata()
-    }, []);
+    }, [page]);
 
 
     const newdata = async() => {
@@ -30,53 +24,58 @@ function Citation () {
      const remoteDBViolation = new PouchDB('http://admin:admin@192.168.0.191:5984/z_violation')
         console.log('remoteDBViolation');
         console.log(remoteDBViolation);
-        console.log('remoteDBViolation');
         var result = await remoteDBViolation.allDocs({
             include_docs: true,
             attachments: true
           });
           if(result.rows){
-              let modifiedArr = result.rows.map(function(item){
-              return item.doc
+                let modifiedArr = result.rows.map(function(item){
+                return item.doc
           });
-          console.log('modifiedArr')
-          console.log(modifiedArr)
-          setOrders(modifiedArr)
-          console.log('modifiedArr')
+                let filteredData = modifiedArr.filter(item => {
+                return item.Status;
+          });
+          if (filteredData) {
+            let newFilterData = filteredData.map(item => {
+              return item;
+            });
+            setContent(newFilterData)
+
+          }
     }
 }
 
     // Search
     const __handleSearch = (event) => {
         setSearch(event.target.value);
-        if (event.target.value !== '') {
-            let search_results = orders.filter((item) =>
-                
-            (new RegExp(searchTerm, 'i').test(item.refNum) ||
-            new RegExp(searchTerm, 'i').test(item.DriverName) 
-            )
-                
+        if (event.target.value) {
+            let search_results = content.filter((item) =>
+                (new RegExp(search, 'i').test(item.refNum) ||
+                new RegExp(search, 'i').test(item.DriverName) ||
+                new RegExp(search, 'i').test(item.ContactNumber)
+                )
             );
-            setOrders(search_results);
+            setContent(search_results);
         }
-        else {
-            __handleChangePage(1);
+        else if (event.target.value == '') {
+            __handleSearch()
         }
     };
 
     // Change Page 
     const __handleChangePage = (new_page) => {
         setPage(new_page);
-        setOrders(sliceData(orders, new_page, 5));
+        setContent(sliceData(content, new_page, 5));
     }
 
-    const TicketList = async(violators) => {
+    const StatusChange = async(violators) => {
 
-        const remoteDBViolation = new PouchDB('http://admin:admin@192.168.100.14:5984/violation')
+        const remoteDBViolation = new PouchDB('http://admin:admin@192.168.0.191:5984/z_violation')
         console.log('dsdsd')
         console.log(violators)
         console.log('dsdsd')
-        remoteDBViolation.get(violators._id).then(function(doc) {
+        remoteDBViolation.get(violators._id)
+        .then(function(doc) {
             return remoteDBViolation.put({
                 _id: doc._id,
               ...doc,
@@ -85,23 +84,12 @@ function Citation () {
           }).then(function(response) {
             console.log('response')
             console.log(response)
-            console.log('response')
-            setOrders(response)
+            setContent(response)
           }).catch(function (err) {
             console.log(err);
           });
           await newdata()
           window.location.reload(true)
-          
-    //    var data =  {...violators, status: "Paid"}
-    //    const dasta = [...orders , data]
-    //    console.log('data')  
-    //    console.log(data)
-    //    console.log('data')
-    //    console.log('dasta')
-    //    console.log(dasta)
-    //    console.log('dasta')
-    //    setOrders(dasta)
     }
 
    
@@ -117,13 +105,17 @@ function Citation () {
             <div className='dashboard-content-container'>
                 <div className='dashboard-content-header'>
                     <h2>Citation List</h2>
-                    <div className='dashboard-content-search'>
+                    <div>
+                    <button onClick = {newdata}>refresh</button>
+                    <div className='dashboard-content-search'>'
+                    
                         <input
                             type='text'
                             value={search}
                             placeholder='Search..'
                             className='dashboard-content-input'
-                            onChange={e => __handleSearch(e)} />
+                            onChange={(item) => __handleSearch(item)} />
+                    </div>
                     </div>
                 </div>
 
@@ -137,16 +129,11 @@ function Citation () {
                         <th>License Plate</th>
                         <th>Vehicle Type</th>
                         <th>Status</th>
-                        {/* <th>ContactNumber</th>
-                        <th>LicenseNumber</th>
-                        <th>LicensePlate</th> */}
-                        {/* <th>VehicleType</th> */}
-                        {/* <th>Violation</th> */}
                     </thead>
 
-                    {orders.length !== 0 ?
+                    {content.length !== 0 ?
                         <tbody>
-                            {orders.map((violators, index) => (
+                            {content.map((violators, index) => (
                                 <tr key={index}>
                                     <td><span>{violators.refNum}</span></td>
                                     <td><span>{violators.DriverName}</span></td>
@@ -155,7 +142,7 @@ function Citation () {
                                     <td><span>{violators.LicenseNumber}</span></td>
                                     <td><span>{violators.LicensePlate}</span></td>
                                     <td><span>{violators.VehicleType}</span></td>
-                                    <button disabled={violators.Status === "Paid" ? true : false} onClick={() => {TicketList(violators)}}>
+                                    <button disabled={violators.Status === "Paid" ? true : false} onClick={() => {StatusChange(violators)}}>
                                     <td><span>{violators.Status}</span></td>
                                     </button>
                                 </tr>
@@ -163,7 +150,7 @@ function Citation () {
                         </tbody>
                     : null}
                 </table>
-                {orders.length !== 0 ?
+                {content.length !== 0 ?
                     <div className='dashboard-content-footer'>
                         {pagination.map((item, index) => (
                             <span 
